4aa13d430199ba01c8ed713fdcbde02e0b65c5a2
Gilthanas122 <pityu.django@gmail.com>
2019-03-07 13:57:11 +0100

    init - working but not searching through subdirectories

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        File folder = new File("/home/pityu/Desktop/findstringrada/src/textfiles");
+        File[] listOfFiles = folder.listFiles();
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a search term: ");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String searchTerm = reader.nextLine();
+        String directoryName = searchTerm + timestamp;
+        File destinationDir = new File("searchResults/" + directoryName);
+        Files.createDirectory(destinationDir.toPath());
+
+
+        if (folder.isDirectory()) {
+            for (File file : listOfFiles) {
+                if (!file.isDirectory()) {
+                    // BufferedReader br = new BufferedReader(new FileReader(file));
+                    List<String> readAllLines = Files.readAllLines(file.toPath());
+                    for (String lines : readAllLines) {
+                        if (lines.contains(searchTerm)) {
+                            System.out.println(file.getName());
+                            File destFile = new File(destinationDir + "/" + file.getName());
+                            copyFileToLocation( file, destFile);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        } else {
+            System.out.println("A Directory!");
+        }
+
+
+    }
+
+
+    private static boolean checkDirectoryName(File theDir) {
+        return theDir.exists();
+    }
+
+    private static void copyFileToLocation(File file, File destination) throws IOException {
+        Path origin = Paths.get(file.getPath());
+        Path destinationPath = Paths.get(destination.getPath());
+        Files.copy(origin, destinationPath);
+    }
+}
